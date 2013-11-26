e27a66f383131e3f3a64dc9169ffaacae97f3564
Cameron Kozinski <camski20@gmail.com>
2013-11-26 17:40:55 -0500

    practice

--- Wk1/goal1_basics/js/main.js
+++ Wk1/goal1_basics/js/main.js
@@ -63,63 +63,106 @@ console.log('------ variables ----------');
 
 
     //example 3: a mix of variables defined and not defined
-    var name, course = "PWA1", month;
-    console.log ("name: ", name + " /course: ", course + " /month: ", month);
+    //var name, course = "PWA1", month;
+    //console.log ("name: ", name + " /course: ", course + " /month: ", month);
 
     //example 4: declaring multiple variables at once w/ same value
-    var a = b = c = d = 10;
-    console.log("a =", a + " /b =", b + " /c =", c + " /d =", d );
+    //var a = b = c = d = 10;
+    //console.log("a =", a + " /b =", b + " /c =", c + " /d =", d );
 
 
 // string (definition, concatenation, escape character)
 console.log('------ strings ----------');
+        var name = "James Bond";
+        console.log(name);
 
     //basic string
-
+    //var testString = "He\'s in PWA1";
+    //console.log("Example 1: ",testString);
+ 
 
     //string w/ escape characters " \ "
+    //var testString = "I need some \"quotes\" to be here.";
+    //console.log("Example 2: ",testString);
+
+    //var testString = 'He\'s in PWA1';
+    //console.log("Example 3: ",testString);
+
+    //var testString = 'He\'s in' + '\"PWA1\"';
+    //console.log("Example 4: ", testString);
 
 
     //string w/ escape characters and mixed single/double quotes
 
 
     //this example shows that all the "phase" vars above were overwritten
+    //var num = 3;
+    //var testString = "he's in PWA1 " + num;
+    //console.log("Example 5: ", testString);
 
 
 
 // numbers (definition, concatenation +, math, numbers & strings)
 console.log('------ numbers ----------');
-
+    
     // arithmetic operators:  +, -, /, *, % (modulo)
 
 
     //basic math
-
+    //var counter = 10;
+    //console.log("basic math: ", counter + 1);
+    //console.log("basic math: ", counter);
 
     //quick operations with "assignment operator" +=, -=, *=, /=, %=
     //one example below, can show more examples with other assignment operators
-
+    //var num1 = 10;
+    //var num2 = 5;
+    //num1 += num2;
+    //console.log("+=: ", num1);
 
     //increment or decrement numbers by 1 with ++ or --
 
+    //var myNum = 5;
+    //console.log("myNum = " ,myNum);
+    //myNum++
+    //console.log("myNum = " ,myNum);
+    //myNum--
+    //console.log("myNum = " ,myNum);
+
     //this decrements right before it is being used
+    //var num =10;
+    //--num;
+    //console.log("--counter: ", num);
+    
 
     //this decrements after it is used
+    //num--;
+    //console.log("counter--: ", num);
+
 
     //comment out the above example
     //this decrements after it is being used as well
 
 
     //add a number to a string is always a string
+    var myStr = "6" + 2;
+    console.log("number + string: ", myStr);
 
 
     //other math operations will result in a number
+    myStr = "6" / 2;
+    console.log("number / string: ", myStr);
 
 
     //order of operations: ( ), *, /, +, -
+    var num = 4 + (6 * 5 - 10) / 5;
+    console.log("order of operations: ", num);
 
 
     //modulo example
+    var num1 = 10;
+    var num2 = 3;
+    console.log("remainder: ", num1%num2);
 
 
 // arrays: (definition, indexing, getter/setter)
