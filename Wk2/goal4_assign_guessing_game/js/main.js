17183d0d9bca10842976968024d44abc1baf024c
Cameron Kozinski <camski20@gmail.com>
2013-12-07 13:40:46 -0500

    Setup my obj array and added a function to find a number from 1 to 10

--- Wk2/goal4_assign_guessing_game/js/main.js
+++ Wk2/goal4_assign_guessing_game/js/main.js
@@ -10,9 +10,27 @@
  * 
  * GUESSING GAME
  */
+ 
 
 //Game variables
-(function ()
+(function (){
 
+//This is my obj var that will hold all info needed for game.
+	var allInfo = {
+	output:document.querySelector("#output"),
+	input:document.querySelector("#input"),
+	button:document.querySelector("button")};
+	console.log(allInfo.output);
+	console.log(allInfo.input);
+	console.log(allInfo.button);
+
+//This is a function that will find a number 1-10
+	var mathRan = function(){
+		var f1 = Math.floor((Math.random()*10)+1);
+		return f1;
+	}
+//this is adding the info taken from the math function and adding it to my obj array
+	allInfo.mathRan = mathRan();
+	console.log(allInfo.mathRan)
 
 })();
