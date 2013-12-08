f36de2a09dc3fb890b560d0c81afa986e0b1bae6
Cameron Kozinski <camski20@gmail.com>
2013-12-07 22:05:48 -0500

    Game complete all bugs found

--- Wk2/goal4_assign_guessing_game/js/main.js
+++ Wk2/goal4_assign_guessing_game/js/main.js
@@ -15,22 +15,109 @@
 //Game variables
 (function (){
 
-//This is my obj var that will hold all info needed for game.
-	var allInfo = {
+
+  var allInfo = {
 	output:document.querySelector("#output"),
 	input:document.querySelector("#input"),
-	button:document.querySelector("button")};
+	button:document.querySelector("button"),
+	mathRan:Math.floor((Math.random()*10)+1)
+	};
+ 	//document.querySelector("#input").placeholder = "Enter your guess:";
 	console.log(allInfo.output);
 	console.log(allInfo.input);
 	console.log(allInfo.button);
+	console.log(allInfo.mathRan);
+	var round = 0; 
+	var remTime = 3;
+	 
+var game = function(){
+	//This is my obj var that will hold all info needed for game.
+	
+	round++
+	remTime--
+	console.log("!!!!!!"+ round);
+	var valiInp = function(){
+		userInp = parseInt(allInfo.input.value);
+		if (isNaN(userInp)){
+			var info = "NaN";
+		}else{
+			var info = userInp;
+		};
+			return info;
+	};
+	var info = valiInp();
+	
+	if(info === "NaN"){
+		console.log("This is not a number please try again");
+		round--
+		allInfo.output.innerHTML = "This is not a number please try again";
+	}else if(round===3 && (info<allInfo.mathRan || info<allInfo.mathRan)){
+		console.log("game over");
+		allInfo.output.innerHTML = "Round: " +round+ " OVER, You loose! The correct answer was " +allInfo.mathRan;
+		button.onclick = function(e){
+		console.log("gameover")
 
-//This is a function that will find a number 1-10
-	var mathRan = function(){
-		var f1 = Math.floor((Math.random()*10)+1);
-		return f1;
-	}
-//this is adding the info taken from the math function and adding it to my obj array
-	allInfo.mathRan = mathRan();
-	console.log(allInfo.mathRan)
+		e.preventDefault();
+		return false;
+		};
+	}else if(round===3 && info===allInfo.mathRan){
+		console.log("You win. just in time.");
+		allInfo.output.innerHTML = "End of round " +round+ "...BUT You WIN! the answer is " +allInfo.mathRan;
+		button.onclick = function(e){
+		console.log("gameover")
 
+		e.preventDefault();
+		return false;
+		};
+	}else if(info===allInfo.mathRan && round === 1){
+		console.log("you win");
+		allInfo.output.innerHTML = "You win on your first try!! the answer was " +allInfo.mathRan;
+		button.onclick = function(e){
+		console.log("gameover")
+
+		e.preventDefault();
+		return false;
+		};
+	}else if(info===allInfo.mathRan){
+		console.log("YOU WIN");
+		allInfo.output.innerHTML = "End of round " +round+ "...BUT You WIN! the answer is " +allInfo.mathRan;
+		button.onclick = function(e){
+		console.log("gameover")
+
+		e.preventDefault();
+		return false;
+		};
+	}else if(info < allInfo.mathRan){
+		
+		console.log("nope not this time");
+		allInfo.output.innerHTML = "That's too low. Round " +round+ " over! You get to try " +remTime+ " more time(s)."
+	}else if(info > allInfo.mathRan){
+		console.log("nope not this time");
+		allInfo.output.innerHTML = "That's too High! Round " +round+ " over! You get to try " +remTime+ " more time(s)."
+		
+	};
+
+	
+  };
+    var button = document.querySelector("button")
+	button.onclick = function(e){
+		game()
+
+		e.preventDefault();
+		return false;
+	};
 })();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
