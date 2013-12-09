b1e8ac4a5748d240ff7a469fa441d02a6c555fdd
Cameron Kozinski <camski20@gmail.com>
2013-12-09 00:44:46 -0500

    Changed both players into one array, and changed all needed info

--- Wk2/goal4_assign_duel3/js/main.js
+++ Wk2/goal4_assign_duel3/js/main.js
@@ -5,17 +5,19 @@
 (function(){
 	console.log("FIGHT");
 //Player info
-	var plOne = {name:"Dumbo",health:100,damage:20};
-	var plTwo = {name:"Colonel Hathi",health:100,damage:20};
+	var plrs = {
+		 plOne:{name:"Dumbo",health:100,damage:20},
+		 plTwo:{name:"Colonel Hathi",health:100,damage:20}
+		};
 	
 //player names/health on screen 
 	var dumboNm = document.querySelector("#kabal p");
 	console.log("!!!!!!",dumboNm);
-	dumboNm.innerHTML = plOne.name +":"+ plOne.health;
+	dumboNm.innerHTML = plrs.plOne.name +":"+ plrs.plOne.health;
 
 	var colHathiNm = document.querySelector("#kratos p");
 	console.log("!!!!!!",colHathiNm);
-	colHathiNm.innerHTML = plTwo.name +":"+ plTwo.health;
+	colHathiNm.innerHTML = plrs.plTwo.name +":"+ plrs.plTwo.health;
 //-----------------------------------------------------------
 
 //Round counter/starting point
@@ -28,17 +30,17 @@
 
 	function fight(){
 		//Formula for finding the amount of damage recieved for each player 
-		var plOneMinDam = plOne.damage * .5;
-		var plTwoMinDam = plTwo.damage * .5;
-		var f1 = Math.floor(Math.random()*(plOne.damage-plOneMinDam)+plOneMinDam);
-		var f2 = Math.floor(Math.random()*(plTwo.damage-plTwoMinDam)+plTwoMinDam);
+		var plOneMinDam = plrs.plOne.damage * .5;
+		var plTwoMinDam = plrs.plTwo.damage * .5;
+		var f1 = Math.floor(Math.random()*(plrs.plOne.damage-plOneMinDam)+plOneMinDam);
+		var f2 = Math.floor(Math.random()*(plrs.plTwo.damage-plTwoMinDam)+plTwoMinDam);
 		
 		//Damage taken from each player and info output via console.log.
-		plOne.health = plOne.health - f1;
-		plTwo.health = plTwo.health - f2;
-		console.log(plOne.name+" : "+plOne.health+ " " +plTwo.name+" : "+plTwo.health);
-		dumboNm.innerHTML = plOne.name +":"+ plOne.health;
-		colHathiNm.innerHTML = plTwo.name +":"+ plTwo.health;
+		plrs.plOne.health = plrs.plOne.health - f1;
+		plrs.plTwo.health = plrs.plTwo.health - f2;
+		console.log(plrs.plOne.name+" : "+plrs.plOne.health+ " " +plrs.plTwo.name+" : "+plrs.plTwo.health);
+		dumboNm.innerHTML = plrs.plOne.name +":"+ plrs.plOne.health;
+		colHathiNm.innerHTML = plrs.plTwo.name +":"+ plrs.plTwo.health;
 		round = round + 1;
 		theBell.innerHTML = "Round "+ round + " is OVER!";
 		console.log(round);
@@ -51,7 +53,7 @@
 	function winnerCheck(){
 		var result = "no winner";
 		//if both players die text will display 
-		if (plOne.health<1 && plTwo.health<1)
+		if (plrs.plOne.health<1 && plrs.plTwo.health<1)
 		{
 			dumboNm.innerHTML="";
 			colHathiNm.innerHTML="You Both Die";
@@ -61,18 +63,18 @@
 			};
 			result = colHathiNm;
 		//if only dumbo's health drops to 0 col wins
-		}else if(plOne.health<1){
+		}else if(plrs.plOne.health<1){
 			dumboNm.innerHTML="";
-			colHathiNm.innerHTML=  plTwo.name + " WINS!!!";
+			colHathiNm.innerHTML=  plrs.plTwo.name + " WINS!!!";
 			fightBtn.innerHTML = "Done!";
 			fightBtn.onclick = function(e){
 				console.log("Game Over");
 			};
 			result = plTwo;
 		//if only col health drops to 0 dumbo wins
-		}else if(plTwo.health<1){
+		}else if(plrs.plTwo.health<1){
 			dumboNm.innerHTML = "";
-			colHathiNm.innerHTML=plOne.name + " WINS!!!";
+			colHathiNm.innerHTML=plrs.plOne.name + " WINS!!!";
 			fightBtn.innerHTML = "Done!";
 			fightBtn.onclick = function(e){
 				console.log("Game Over");
